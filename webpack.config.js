c2a8e8bb320a36c18070e5c28c6d23fa434bce00
Squishy123 <christian.gnaw@gmail.com>
2019-12-16 19:28:25 -0500

    Added project init with webpack config

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 8000
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Project REKT Engine | DEVELOPMENT"
+        })
+    ]
+};
