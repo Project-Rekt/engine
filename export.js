79d0bf1a0eb1ab6a221f33dae3bb0cc0ec5d2e53
Christian Wang <christian.gnaw@gmail.com>
2020-01-19 16:51:12 -0500

    Updates

--- export.js
+++ export.js
@@ -15,9 +15,16 @@ Object.defineProperty(exports, "InputHandler", {
     return _inputHandler["default"];
   }
 });
+exports["default"] = void 0;
 
 var _actor = _interopRequireDefault(require("./lib/actor"));
 
 var _inputHandler = _interopRequireDefault(require("./lib/inputHandler"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var _default = {
+  Actor: _actor["default"],
+  InputHandler: _inputHandler["default"]
+};
+exports["default"] = _default;
