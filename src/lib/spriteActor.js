64253463d9b6b65db27b837e3cc89cf09c059537
Elijah <elijah.tungul@ryerson.ca>
2020-02-01 22:29:53 -0500

    Overridden skeleton functions and other various changes

--- src/lib/spriteActor.js
+++ src/lib/spriteActor.js
@@ -9,20 +9,42 @@ async function loadImage(src) {
 }
 
 export default class SpriteActor extends Actor{
-    constructor(bounds, properties, states){
+    constructor(bounds, spriteObj){
         super(bounds);
-        this.properties = properties || {};
-        this.states = states || {idle: {location: null, rps: 5, loop: true, frames: [{x: 0, y: 0, width: 0, height: 0}]}};
+        this.properties = spriteObj.properties || {};
+        this.states = spriteObj.states || {idle: {location: null, rps: 5, loop: true, frames: [{x: 0, y: 0, width: 0, height: 0}]}};
 
     }
 
-    loadImage = () => {
-    (async () => {
-        await Promise.all(Object.keys(sprites.states).map(async (s) => {
-            let img = await loadImage(sprites.states[s].location);
-            Object.assign(sprites.states[s], { image: img });
-        }));
-        console.log(sprites);
-    })()};
-    
+    loadSpriteObj = (spriteObj) => {
+        this.properties = spriteObj.properties;
+        this.states = spriteObj.states;
+
+        //what is diffing images?
+    }
+
+    create = () => {
+        (async () => {
+            await Promise.all(Object.keys(sprites.states).map(async (s) => {
+                let img = await loadImage(sprites.states[s].location);
+                Object.assign(sprites.states[s], { image: img });
+            }));
+            console.log(sprites);
+        })()
+    }
+
+    render = (deltaTime) => {
+        if(this.states[0].loop == true){
+            let numFrames = this.states[0].frames.length;
+            for(let i = 0; i<numFrames; i++){
+                this.bounds = this.states[0].frames[i];
+            }
+        }
+    }
+
+    destroy = () => {
+        super.destroy();
+    }
+
+
 }
