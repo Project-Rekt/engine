105cb7899dcb1064ee74d2ae4cc29dfc9d074958
Christian Wang <christian.gnaw@gmail.com>
2020-02-03 10:28:41 -0500

    parent 2e26dc86fc366bebe27e574b43a2a8bda7137c12 author Christian Wang <christian.gnaw@gmail.com> 1580743721 -0500 committer Christian Wang <christian.gnaw@gmail.com> 1580927501 -0500
    Added create and destroy lifecycle calls for engine
    
    Added fixes and formatter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-console.log("%cMASON ENGINE:%cLOADED!", 
-"background: cornflowerblue; color: white; font-size: 35px;", 
-"background: green; color: white; font-size: 35px;");
+console.log(
+    "%cMASON ENGINE:%cLOADED!",
+    "background: cornflowerblue; color: white; font-size: 35px;",
+    "background: green; color: white; font-size: 35px;"
+)
--- tests/bouncingBall/main.js
+++ tests/bouncingBall/main.js
@@ -1,34 +1,39 @@
-import Stage from '../../src/lib/stage';
-import Actor from '../../src/lib/actor';
-import InputHandler from '../../src/lib/inputHandler';
+import Stage from "../../src/lib/stage"
+import Actor from "../../src/lib/actor"
+import InputHandler from "../../src/lib/inputHandler"
 
 class Platform extends Actor {
     constructor(bounds) {
-        super(bounds);
+        super(bounds)
     }
 
     render = () => {
-        this.ctx.fillStyle = "white";
-        this.ctx.fillRect(this.bounds.x, this.bounds.y, this.bounds.width, this.bounds.height);
+        this.ctx.fillStyle = "white"
+        this.ctx.fillRect(
+            this.bounds.x,
+            this.bounds.y,
+            this.bounds.width,
+            this.bounds.height
+        )
     }
 }
 
 class Ball extends Actor {
     constructor(bounds) {
-        super(bounds);
-        this.bounds.width = 10;
-        this.bounds.height = 10;
-        this.ay = 0.05;
-        this.ax = 0.001;
+        super(bounds)
+        this.bounds.width = 10
+        this.bounds.height = 10
+        this.ay = 0.05
+        this.ax = 0.001
 
-        this.vx = 0;
-        this.vy = 0;
+        this.vx = 0
+        this.vy = 0
 
-        this.px = this.bounds.x;
-        this.py = this.bounds.y;
+        this.px = this.bounds.x
+        this.py = this.bounds.y
     }
 
-    render = (dt) => {
+    render = dt => {
         //clearframe
         this.ctx.fillStyle = "black";
         this.ctx.fillRect(this.px, this.py, this.bounds.width, this.bounds.height);
@@ -36,39 +41,42 @@ class Ball extends Actor {
         this.py = Math.round(this.bounds.y);
 
         //drawframe
-        this.ctx.fillStyle = "#39ff14 ";
-        this.ctx.fillRect(this.px, this.py, this.bounds.width, this.bounds.height);
+        this.ctx.fillStyle = "#39ff14 "
+        this.ctx.fillRect(
+            this.px,
+            this.py,
+            this.bounds.width,
+            this.bounds.height
+        )
     }
 
-    update = (dt) => {
-        //check bounce 
+    update = dt => {
+        //check bounce
         // if (this.bounds.y >= 590) {
-        let collisions = this.stage.getCollisions(this);
-        let p = collisions.find(c => c instanceof Platform);
+        let collisions = this.stage.getCollisions(this)
+        let p = collisions.find(c => c instanceof Platform)
         if (p) {
             console.log("BOOM")
-            if (this.vy > 0)
-                this.bounds.y = p.bounds.y - this.bounds.height;
-            else
-                this.bounds.y = p.bounds.y + this.bounds.height;
-            this.vy *= -0.75;
+            if (this.vy > 0) this.bounds.y = p.bounds.y - this.bounds.height
+            else this.bounds.y = p.bounds.y + this.bounds.height
+            this.vy *= -0.75
         }
 
         //update position
-        this.vy += this.ay;
-        this.bounds.y += this.vy;
+        this.vy += this.ay
+        this.bounds.y += this.vy
 
-        this.vx += this.ax;
-        this.bounds.x += this.vx;
+        this.vx += this.ax
+        this.bounds.x += this.vx
     }
 }
 
-let stage = new Stage(document.querySelector('#main'));
-let inp = new InputHandler(document.querySelector('#main'), {
-    mousedown: function () {
-        stage.addActor(new Ball({ x: this.input.x, y: this.input.y }), 1);
-    }
-});
+let stage = new Stage(document.querySelector("#main"))
+let inp = new InputHandler(document.querySelector("#main"), {
+    mousedown: function() {
+        stage.addActor(new Ball({ x: this.input.x, y: this.input.y }), 1)
+    },
+})
 stage.addActor(new Platform({ x: 150, y: 300, width: 300, height: 10 }))
 stage.addActor(new Platform({ x: 50, y: 400, width: 300, height: 10 }))
 stage.addActor(new Platform({ x: 100, y: 500, width: 300, height: 10 }))
