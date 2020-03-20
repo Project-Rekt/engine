c4b8a529401ce6289e00258d0df7bd9ac7885b65
Christian Wang <christian.gnaw@gmail.com>
2020-03-20 11:36:58 -0400

    Updates

--- export.js
+++ export.js
@@ -5,9 +5,11 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.InputHandler = exports.Stage = exports.Actor = exports["default"] = void 0;
+exports.SpriteActor = exports.InputHandler = exports.Stage = exports.Actor = exports["default"] = void 0;
 
 var _actor = _interopRequireDefault(require("./lib/actor"));
+
+var _spriteActor = _interopRequireDefault(require("./lib/spriteActor"));
 
 var _stage = _interopRequireDefault(require("./lib/stage"));
 
@@ -16,12 +18,15 @@ var _inputHandler = _interopRequireDefault(require("./lib/inputHandler"));
 var _default = {
   Actor: _actor["default"],
   InputHandler: _inputHandler["default"],
-  Stage: _stage["default"]
+  Stage: _stage["default"],
+  SpriteActor: _spriteActor["default"]
 };
 exports["default"] = _default;
 var Actor = _actor["default"],
     Stage = _stage["default"],
-    InputHandler = _inputHandler["default"];
+    InputHandler = _inputHandler["default"],
+    SpriteActor = _spriteActor["default"];
+exports.SpriteActor = SpriteActor;
 exports.InputHandler = InputHandler;
 exports.Stage = Stage;
 exports.Actor = Actor;
