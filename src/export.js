997373592f9f33a261098405364674c9ac264953
Christian Wang <christian.gnaw@gmail.com>
2020-03-20 11:37:32 -0400

    Fixed export link and increment properties

--- src/export.js
+++ src/export.js
@@ -1,11 +1,13 @@
 import actor from './lib/actor'
+import spriteactor from './lib/spriteActor'
 import stage from './lib/stage'
 import inputHandler from './lib/inputHandler'
 
 export default {
     Actor: actor,
     InputHandler: inputHandler,
-    Stage: stage
+    Stage: stage,
+    SpriteActor: spriteactor
 };
 
-export let Actor = actor, Stage = stage, InputHandler = inputHandler;
+export let Actor = actor, Stage = stage, InputHandler = inputHandler, SpriteActor = spriteactor;
